ef507d24d7bf5ba2d7713d85428e40f44b25f0ba
Thomas-Frew <thomasfrew13@gmail.com>
2024-09-05 12:07:12 +0930

    Fixed minor bug with public chat serialisation

--- python_server/server.py
+++ python_server/server.py
@@ -240,7 +240,7 @@ class Server:
         
         # Send public chat message to all clients
         for _, client_socket in self.clients.items():
-            await client_socket.send(message)
+            await client_socket.send(json.dumps(message))
             
     async def handle_client_list_request(self, websocket):
         """ Handle CLIENT_LIST_REQUEST messages (respond with CLIENT_LIST). """
