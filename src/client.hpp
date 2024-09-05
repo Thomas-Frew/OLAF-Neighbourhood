8ca0e034d36135b322ae37745f125ea98f752baf
Jack <104746198+mortonjack@users.noreply.github.com>
2024-09-05 22:02:50 +0930

    format

--- src/client.hpp
+++ src/client.hpp
@@ -2,15 +2,15 @@
 #include <string>
 
 class Client {
-public:
-    Client(std::string public_key): m_public_key(public_key), m_counter(0) {}
+  public:
+    Client(std::string public_key) : m_public_key(public_key), m_counter(0) {}
 
     std::string getPublicKey();
     uint32_t getCounter();
 
     void run();
 
-private:
+  private:
     std::string m_public_key;
     uint32_t m_counter;
 };
